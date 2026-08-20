8324b83d15e462f65a53385747ebe48cf9d15f6d
Andrew Crowther <hhmission2018@gmail.com>
2026-08-20 07:06:13 -0600

    ToDoList v1

---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime? DueDate { get; set; }
+        public enum Priority {Low, Medium, High}
+    }
+
+
+}
